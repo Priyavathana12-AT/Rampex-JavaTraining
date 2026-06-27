0cd59378bd176771f733888dd1cc25383ec497d8
Priyavathana <priyavathanaanandhan@gmail.com>
2026-06-27 09:48:29 +0800

    code created

---
+++ src/DAY6/classwork/Star.java
@@ -0,0 +1,13 @@
+package DAY6.classwork;
+
+class Patternprinting {
+    public static void main(String[] args){
+        for(int i = 5; i > 0; i--){
+            for(int j = 1; j <= i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+}
