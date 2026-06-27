dce355197549f21bc9aee5d84d4a053ca52c6051
Priyavathana <priyavathanaanandhan@gmail.com>
2026-06-27 10:00:37 +0800

    code created

---
+++ src/DAY1/LargestOfThree.java
@@ -0,0 +1,23 @@
+package DAY1;
+
+class Number {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = 15;
+
+        if (a > b) {
+            if (b > c) {
+                System.out.println("a is the largest");
+            } else {
+                System.out.println("c is the largest");
+            }
+        } else {
+            if (b > c) {
+                System.out.println("b is the largest");
+            } else {
+                System.out.println("c is the largest");
+            }
+        }
+    }
+}
