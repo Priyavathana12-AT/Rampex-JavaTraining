c4ada4dbf0ec25e91b014b095b7010674c63dfed
Priyavathana <priyavathanaanandhan@gmail.com>
2026-06-28 22:37:36 +0800

    code created

---
+++ src/DAY7HOMEWORK/Sumofdigits.java
@@ -0,0 +1,16 @@
+package DAY7HOMEWORK;
+
+public class Sumofdigits {
+    public static void main(String[] args){
+        int num =543;
+        int sum=0;
+        while(num>0){
+            int digit=num%10;
+            sum+=digit;
+            num/=10;
+
+
+        }
+        System.out.print(sum);
+    }
+}
