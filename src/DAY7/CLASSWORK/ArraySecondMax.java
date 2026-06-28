9ce07db6a3d0d6adbd818fb1342eb5e1bc530fd7
Priyavathana <priyavathanaanandhan@gmail.com>
2026-06-28 16:45:53 +0800

    code created

--- src/DAY7/CLASSWORK/ArraySecondMax.java
+++ src/DAY7/CLASSWORK/ArraySecondMax.java
@@ -2,13 +2,13 @@ package DAY7.CLASSWORK;
 
 public class ArraySecondMax {
     public static void main(String[] args){
-        int arr []={1,2,3,4,5};
+        int nums[]={1,2,3,4,5};
         int Smax=0;
         int max=0;
-        for (int i=0;i<arr.length;i++){
-            if(arr[i]>max){
+        for (int i=0;i<nums.length;i++){
+            if(nums[i]>max){
                 Smax=max;
-                max=arr[i];
+                max=nums[i];
             }
         }
         System.out.println("the second maximun value: "+Smax);
