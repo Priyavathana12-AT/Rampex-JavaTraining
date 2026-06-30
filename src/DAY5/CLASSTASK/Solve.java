3ec68c3996e473725221853071d6be6f31f23088
Priyavathana <priyavathanaanandhan@gmail.com>
2026-06-30 11:13:38 +0800

    code created

---
+++ src/DAY5/CLASSTASK/Solve.java
@@ -0,0 +1,9 @@
+package DAY5.CLASSTASK;
+
+public class Solve{
+    public static void main(String[] args){
+        for(int i = 0; i < 7; i++) {
+            System.out.println( i);
+        }
+    }
+}
