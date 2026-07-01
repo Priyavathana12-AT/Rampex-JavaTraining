dadde04a092c1a41d03c5bdd738946f859cae793
Priyavathana <priyavathanaanandhan@gmail.com>
2026-07-01 10:26:49 +0800

    code created

---
+++ src/DAY10/CLASSTASK/Car.java
@@ -0,0 +1,16 @@
+package DAY10.CLASSTASK;
+
+public class Car {
+    String brand;
+    String model;
+    String color;
+
+    public void drive() {
+        System.out.println("Driving");
+    }
+
+    public static void main(String[] args) {
+        Car C1 = new Car();
+        C1.drive();
+    }
+}
