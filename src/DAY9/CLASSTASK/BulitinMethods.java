39b82f31c0bc54d13e9f7cc47536b3e9433e87cb
Priyavathana <priyavathanaanandhan@gmail.com>
2026-07-09 11:39:36 +0530

    code created

---
+++ src/DAY9/CLASSTASK/BulitinMethods.java
@@ -0,0 +1,14 @@
+package DAY9.CLASSTASK;
+
+class BuiltinMethods {
+
+        public static void main(String [] args){
+            String  name ="Mazza";
+
+//       System.out.println(name.toUpperCase());
+//       System.out.println(name);
+//       System.out.println(name.replaceAll("M","L"));
+            System.out.println(name.equalsIgnoreCase("mazza"));
+
+        }
+    }
