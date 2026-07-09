17d5a8864f62355c6c5d47d74c621b552bc3ad01
Priyavathana <priyavathanaanandhan@gmail.com>
2026-07-09 10:33:24 +0530

    code created

---
+++ src/DAY11/CLASSTASK/CheckAmstrong.java
@@ -0,0 +1,7 @@
+package DAY11.CLASSTASK;
+
+public class CheckAmstrong {
+    public static void main(String[] args){
+        System.out.println("A");
+    }
+}
---
+++ src/DAY9/CLASSTASK/ParallelArray.java
@@ -0,0 +1,11 @@
+package DAY9.CLASSTASK;
+
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class ParallelArray {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+    }
+}
